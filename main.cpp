e38249959cdcffe51812551acec80583cce8415e
Peter Lu <mcfloundinho@gmail.com>
2016-04-23 15:25:53 +0800

    add command line argument parser

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+char* getCmdOption(char** begin, char** end, const string& option);
+bool cmdOptionExists(char** begin, char** end, const string& option);
+void printHelp(char* name);
+
+int main(int argc, char* argv[])
+{
+    if (cmdOptionExists(argv, argv + argc, "-h")) {
+        printHelp(argv[0]);
+        return 0;
+    }
+    if (!cmdOptionExists(argv, argv + argc, "-b") || !cmdOptionExists(argv, argv + argc, "-s")
+        || !cmdOptionExists(argv, argv + argc, "-r") || !cmdOptionExists(argv, argv + argc, "-c")) {
+        printHelp(argv[0]);
+        return 0;
+    }
+    if (!getCmdOption(argv, argv + argc, "-b") || !getCmdOption(argv, argv + argc, "-s")
+        || !getCmdOption(argv, argv + argc, "-r") || !getCmdOption(argv, argv + argc, "-c")) {
+        printHelp(argv[0]);
+        return 0;
+    }
+    string bigImageFileName = getCmdOption(argv, argv + argc, "-b");
+    string smallImageDirName = getCmdOption(argv, argv + argc, "-s");
+    int numRow = -1, numColumn = -1;
+    stringstream ss;
+    ss << getCmdOption(argv, argv + argc, "-r");
+    ss >> numRow;
+    ss.clear();
+    ss << getCmdOption(argv, argv + argc, "-c");
+    ss >> numColumn;
+    if (numRow <= 0 || numColumn <= 0) {
+        printHelp(argv[0]);
+        return 0;
+    }
+    // TODO
+    return 0;
+}
+
+char* getCmdOption(char** begin, char** end, const string& option)
+{
+    char** itr = find(begin, end, option);
+    if (itr != end && ++itr != end) {
+        return *itr;
+    }
+    return 0;
+}
+
+bool cmdOptionExists(char** begin, char** end, const string& option)
+{
+    return find(begin, end, option) != end;
+}
+
+void printHelp(char* name)
+{
+    cout << "Usage: " << name << " "
+         << "-b BIG_IMAGE_FILE_NAME "
+         << "-s SMALL_IMAGE_DIR "
+         << "-r NUM_COLUMN "
+         << "-c NUM_ROW"
+         << endl;
+}
